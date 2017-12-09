8c9ca7a7b7b4d8d73f355ff1c44e701b1af14381
solaxun <solaxun@hotmail.com>
2017-12-09 00:19:44 -0800

    add day 9

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
 import collections
 import itertools
+import sys
 
+sys.setrecursionlimit(10000)
 
 #os.chdir('../../aoc2017')
 print(os.getcwd())
 print(os.listdir())
+data = open('day9.txt').read().strip()
+
+def process_stream(s,val=1):
+    if not s: return 0
+    first, rest = s[0], s[1:]
+    if first == '{':
+        return val + process_stream(rest,val + 1)
+    elif first == '}':
+        return process_stream(rest, val - 1)
+    else:
+        return process_stream(rest,val)
+
+print(process_stream(data))
